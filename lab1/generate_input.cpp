02d9b2404d607764139f2c79da91402122da2019
alexflorian456 <126677373+alexflorian456@users.noreply.github.com>
2023-10-24 17:50:08 +0300

    Update generate_input.cpp

--- lab1/generate_input.cpp
+++ lab1/generate_input.cpp
@@ -1,19 +1,18 @@
 #include<random>
 #include<iostream>
 #include<fstream>
+#include<cstring>
 
 using namespace std;
 
-int main(){
+int main(int argc, char** argv){
     int n,m;
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<int> distribution(1,100);
-    ofstream g("date.txt");
-    cout<<"Linii: ";
-    cin>>n;
-    cout<<"Coloane: ";
-    cin>>m;
+    ofstream g(argv[3]);
+    n = atoi(argv[1]);
+    m = atoi(argv[2]);
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             g << distribution(gen) << " ";
